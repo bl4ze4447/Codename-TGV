6b8f7d0c5d49ca53129937601ae48241400241a8
Belu D. Antonie-Gabriel <belutoni06@gmail.com>
2024-11-04 12:21:13 +0200

    Changed the building and printing of the hex number
    Build the string and make use of print_string

--- nasm/utilities/numbers.asm
+++ nasm/utilities/numbers.asm
@@ -1,43 +1,45 @@
 ; start
 print_hex:
     pusha
-    mov ah, 0x0e
-    push 16 ; mark stack end
+    mov ah, 5 ; end of HEX_OUT
     jmp get_hex_remainder
 
 get_hex_remainder:
     ; push remainder
-    mov al, [bx]
+    mov al, [dx]
     and al, 0x0F
-    push al
+    call convert_to_ascii
+    mov [HEX_OUT+ah], al
+    sub ah, 1
+    
     ; divide by 16
-    shr [bx], 4
+    mov al, [dx]
+    shr al, 4
+    mov [dx], al
     ; compare
-    cmp [bx], 0
+    cmp [dx], 0
     jne get_hex_remainder
-
-print_hex_check:
-    pop al
-    cmp al, 16 ; is stack end?
-    jne print_hex_digit
     je print_hex_end
 
-print_hex_digit:
+convert_to_ascii:
     cmp al, 9
     jg digit_to_char ; >9
     jle digit_to_digit ; <=9
     
 digit_to_char:
     add al, 55
-    int 0x10
-    jmp print_hex_check
+    ret
     
 digit_to_digit
     add al, 48
-    int 0x10
-    jmp print_hex_check
+    ret
 
 print_hex_end:
+    mov bx, HEX_OUT
+    print_string
     popa
     ret
 ; end
+
+HEX_OUT:
+    db '0x0000', 0

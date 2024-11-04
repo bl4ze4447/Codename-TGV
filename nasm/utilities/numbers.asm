f809a45f370618cc20d67b489a6f93478e24e4b4
Belu D. Antonie-Gabriel <belutoni06@gmail.com>
2024-11-04 11:06:21 +0200

    Fixed a lot of bugs surrounding print_hex
    Added the printing of numbers higher than 9; Fixed logic bugs; Completed and tested code on paper, tonight at 18:00 Bucharest will be a new commit to this file if there are still hidden bugs.

--- nasm/utilities/numbers.asm
+++ nasm/utilities/numbers.asm
@@ -2,25 +2,39 @@
 print_hex:
     pusha
     mov ah, 0x0e
-    push 0
-    jmp hex_loop
+    push 16 ; mark stack end
+    jmp get_hex_remainder
 
-hex_loop:
-    shr bx, 4
+get_hex_remainder:
     mov al, [bx]
-    and al, 3
+    and al, 15
     push al
+    shr [bx], 4
     cmp [bx], 0
-    jne hex_loop
+    jne get_hex_remainder
 
-print_hex_loop:
+print_hex_check:
     pop al
+    cmp al, 16 
+    jne print_hex_digit
+    je print_hex_end
+
+print_hex_digit:
+    cmp al, 9
+    jg digit_to_char
+    jnge digit_to_digit
+    
+digit_to_char:
+    add al, 30
     int 0x10
-    cmp al, 0
-    jne print_hex_loop
+    jmp print_hex_check
+    
+digit_to_digit
+    add al, 60
+    int 0x10
+    jmp print_hex_check
 
 print_hex_end:
     popa
     ret
-    
 ; end
